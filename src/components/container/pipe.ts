06689ebbbb3ba0e833850dc884c251676298dc2a
edinelsonslima <edinelsonlima86@gmail.com>
2022-06-02 14:42:50 -0300

    created components for game

---
+++ src/components/container/pipe.ts
@@ -0,0 +1,5 @@
+const pipe = document.createElement('img');
+pipe.src = './src/assets/pipe.png';
+pipe.classList.add('pipe');
+
+export { pipe };
---
+++ src/components/container/score.ts
@@ -0,0 +1,5 @@
+const score = document.createElement('h2');
+score.classList.add('score');
+score.innerHTML = 'SCORE 0';
+
+export { score };
